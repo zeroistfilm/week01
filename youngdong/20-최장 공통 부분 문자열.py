01a43494416c2d1408e0f6af2988c60f7e7c3a6f
zeroistfilm <zeroistfilm@naver.com>
2020-12-15 22:27:35 +0900

    yd

--- youngdong/20-최장 공통 부분 문자열.py
+++ youngdong/20-최장 공통 부분 문자열.py
@@ -3,16 +3,56 @@
 A = input()
 B=input()
 
-if len(A)>len(B):
-    long=A
-    short=B
-else:
-    long=B
-    short=A
-for i in range(len(long)):
-    if short in long:
-        break
-    short=short[1:]
-print(short)
+list=[['-' for i in range(len(A))] for i in range(len(B))]
+
+
+for i in range(len(A)):
+    for j in range(len(B)):
+        if A[i]==B[j]:
+            list[j][i]='O'
+
+# for i in list:
+#     print(i)
+
+count=0
+output=[]
+def find_str(i,j): #i=x j=y
+    global count,x,y
+    # print(j,len(B),i,len(A))
+    if j >= len(B)-1 or i >= len(A)-1:
+        return 0
+
+    if list[j + 1][i + 1]=='-' :
+        return 0
+
+    if list[j + 1][i + 1]=='O':
+        count+=1
+        find_str(i+1, j+1)
+    output.append([y,count])
+    # print('index',x,y,'length of word: ',count)
+
+    count=0
+
+for i in range(len(A)):
+    for j in range(len(B)):
+        global x,y
+        x=i
+        y=j
+        find_str(i, j)
+
+max=0
+index=0
+for i in range(len(output)):
+    if max==0:
+        max=output[i][1]
+        index = output[i][0]
+    if max<output[i][1]:
+        max = output[i][1]
+        index = output[i][0]
+
+print(max)
+print(B[index+1:max+index+1])
+
+
 
 
